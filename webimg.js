03e998cfe5169a95ec107b14a444f5dc880d3b6f
Jonas Almeida <jalmeida@mathbiol.org>
2016-05-23 15:26:25 -0400

    showing off dynamic compilation

--- webimg.js
+++ webimg.js
@@ -1,5 +1,10 @@
 console.log('webimg.js loaded !')
 x = [1,2,3,4,5]
-y = x.map(function(xi){
-    return xi*2
+y = x.map(function(xi,i){
+    var li = document.createElement('li')
+    var y=xi*3
+    li.textContent=i+') '+y
+    document.body.appendChild(li)
+    return y
 })
+
